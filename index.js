5e2e33dbaee809613272123090a2c0870f5faa24
Andr0id16 <80408640+Andr0id16@users.noreply.github.com>
2021-10-10 13:52:13 +0530

    linked all webpages and added highlighting of current page(in navbar)

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// var head=document.querySelector("head");
-// head.innerHTML+=`<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-// <link rel="icon" favicon-32x32.png">
-// <link rel="icon"  href="/favicon-16x16.png">
-// <link rel="manifest" href="/site.webmanifest">`;
-// head.innerHTML+='<link rel="shortcut icon" href="favicon.ico"/>';
-window.onscroll=()=>{sticknav();};
+var headhtml=`<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0" />
+    <title>WIKI</title>
 
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="fontawesome-free-5.15.4-web/css/all.css">`;
+window.onload=function(){
+    var head = document.querySelector("head");
+    head.innerHTML=headhtml;
+};    
+window.onscroll=()=>{sticknav();};
 var nav=document.getElementById("nav");
 var sticky = nav.offsetTop;
 function sticknav()
