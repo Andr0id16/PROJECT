8208245dd3af5cbb959c818d982ea18b29f1bb33
Andr0id16 <80408640+Andr0id16@users.noreply.github.com>
2021-10-11 13:27:36 +0530

    removed some unnecessary code

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-var headhtml=`<meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=<device-width>, initial-scale=1.0" />
-    <title>WIKI</title>
-
-    <link rel="stylesheet" href="index.css">
-    <link rel="stylesheet" href="fontawesome-free-5.15.4-web/css/all.css">`;
-window.onload=function(){
-    var head = document.querySelector("head");
-    head.innerHTML=headhtml;
-};    
 window.onscroll=()=>{sticknav();};
 var nav=document.getElementById("nav");
 var sticky = nav.offsetTop;
