9982f93d25caac44ccc169b3536cf7331aff970d
Andr0id16 <80408640+Andr0id16@users.noreply.github.com>
2021-10-14 18:02:06 +0530

    made footer a react component in footer.js

---
+++ footer.js
@@ -0,0 +1,20 @@
+var destination=document.querySelector(".footer-container");
+
+
+class Footer extends React.Component{
+    render()
+    {
+        var liststyle={paddingLeft:"0"}
+        return(
+        <div className="footer">
+      <a href="Aboutus.html">About us</a><br/>
+      <a href="Contactus.html">Contact us</a><br/>
+      <ul type="none" style={liststyle}>
+        <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+        <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+        <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+      </ul>
+      </div>);
+    }
+}
+ReactDOM.render(<Footer/>,destination);
--- main.js
+++ main.js
@@ -20,15 +20,15 @@ function game_animate() {
     else if (downPressed && player.y<(canvas.height-50)) player.y += player.speed;
     ctx.fillStyle="black";
     player.draw(ctx);
-    ctx.fillStyle = 'black';
-    for(let i=0;i<5;i++)
-    {
-    let e=enemies[i];
-    e.y+=Math.random()*e.speed*0.5;
-    e.x+=Math.random()*e.speed*0.5;
-    console.log(e);
-    e.draw(ctx);
-    }
+    // ctx.fillStyle = 'black';
+    // for(let i=0;i<5;i++)
+    // {
+    // let e=enemies[i];
+    // e.y+=Math.random()*e.speed*0.5;
+    // e.x+=Math.random()*e.speed*0.5;
+    // console.log(e);
+    // e.draw(ctx);
+    // }
     
     
     
