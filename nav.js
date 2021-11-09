4c04d0259cd788a74fee161c20f0d53ed78172a8
Andr0id16 <80408640+Andr0id16@users.noreply.github.com>
2021-11-09 10:33:10 +0530

    made many changes

---
+++ nav.js
@@ -0,0 +1,33 @@
+var destination = document.querySelector("#nav");
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>
+          <a href="index.html">
+            <i className="fas fa-keyboard"></i>
+          </a>
+          <a href="tutorial.html">
+            <i className="fas fa-info"></i>
+          </a>
+          <a href="leaderboards.html">
+            <i className="fas fa-crown"></i>
+          </a>
+          <a href="store.html">
+            <i className="fas fa-shopping-bag"></i>
+          </a>
+          <a href="login.html">
+            <i className="fas fa-user"></i>
+          </a>
+        </span>
+        <span style={{ float: "right" }}>
+          <a href="signup.html">
+            <i className="fas fa-sign-in-alt"></i>
+          </a>
+        </span>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Nav />, destination);
--- signin.js
+++ signin.js
@@ -1,51 +1,51 @@
-let submit=document.getElementById("submit");
-let username=document.getElementById("username");
-let email=document.getElementById("email");
-var password=document.getElementById("password");
+let submit = document.getElementById("submit");
+let username = document.getElementById("username");
+let email = document.getElementById("email");
+var password = document.getElementById("password");
 
-var usernameformat=new RegExp(/^[a-zA-Z0-9]{4,15}$/);
-var emailformat=new RegExp(/^[a-zA-Z0-9\.]{4,30}@[a-zA-Z0-9\.]{4,12}\.(com)|(edu)|(org)$/);
+var usernameformat = new RegExp(/^[a-zA-Z0-9]{4,15}$/);
+var emailformat = new RegExp(
+  /^[a-zA-Z0-9\.]{4,30}@[a-zA-Z0-9\.]{4,12}\.(com)|(edu)|(org)$/
+);
 
-var error_message=[];
-submit.addEventListener("click",function(e){
-    error_message=[];
-    var s=document.querySelectorAll("span");
-    for(var k=0;k<s.length;k++)
-    {
-        s[k].textContent="";
+var error_message = [];
+submit.addEventListener(
+  "click",
+  function (e) {
+    error_message = [];
+    var s = document.querySelectorAll("span");
+    for (var k = 0; k < s.length; k++) {
+      s[k].textContent = "";
     }
     generateError();
-    if(error_message)
-    {   
-        for(var i=0;i<error_message.length;i++)
-        {   
-            let j=error_message[i];
-            var ele=document.getElementById(j);
-            var next=ele.nextElementSibling;
-            console.log(next);
-            next.textContent='❌';
-        }    
+    if (error_message) {
+      for (var i = 0; i < error_message.length; i++) {
+        let j = error_message[i];
+        var ele = document.getElementById(j);
+        var next = ele.nextElementSibling;
+        console.log(next);
+        next.textContent = "❌";
+      }
+    } else {
+      document.getElementById("#signup").reset();
     }
-    else{
-        document.getElementById('#signup').reset();
-    }
-    
+
     e.preventDefault();
-},true)
+  },
+  true
+);
 
-function generateError(){
-    if(!usernameformat.test(username.value))
-    {
-        error_message.push("username");
-    }
-    if(!emailformat.test(email.value))
-    {
-        error_message.push("email");
-
-    }
-    if(document.getElementById("password").value!==document.getElementById("cpassword").value)
-    {
-        error_message.push("cpassword");
-    }
-    
+function generateError() {
+  if (!usernameformat.test(username.value)) {
+    error_message.push("username");
+  }
+  if (!emailformat.test(email.value)) {
+    error_message.push("email");
+  }
+  if (
+    document.getElementById("password").value !==
+    document.getElementById("cpassword").value
+  ) {
+    error_message.push("cpassword");
+  }
 }
